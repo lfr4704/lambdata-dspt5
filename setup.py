b1c971a31bec748665c6897d24eb50a1686f64b0
Luis Robles <lfr4704@gmail.com>
2020-05-06 19:37:20 -0700

    preparing to release to PYPI

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="my-package-name", # the name that you will install via pip
+    name="my-lambdata-pt5", # the name that you will install via pip
     version="1.0",
-    author="Your Full Name",
-    author_email="your@email.com",
-    description="A short description",
+    author="Luis Robles",
+    author_email="lfr4704@gmail.com",
+    description="My first python package",
     long_description=long_description,
     long_description_content_type="text/markdown", # required if using a md file for long desc
-    #license="MIT",
+    license="MIT",
     url="https://github.com/lfr4704/lambdata-dspt5",
     #keywords="",
     packages=find_packages() # ["my_lambdata"]
