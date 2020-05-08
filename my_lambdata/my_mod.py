af28705f0f454cf567a0ce6554766175c94661dd
Luis Robles <lfr4704@gmail.com>
2020-05-07 20:01:26 -0700

    new function for assignment

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -3,7 +3,7 @@ def enlarge(n):
     return n * 100
 
 if __name__ == "__main__":
-    #only row the code below if executing this script from the cmd line
+    #only run the code below if executing this script from the cmd line
     #otherwise do not run it (for example if we are trying to import something)
     x = 5
     print(enlarge(x))
